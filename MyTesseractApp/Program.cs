3a35d813e2eca2b0545e8e7b2ba0a2d2622cd9a5
Connor <wehrum.connor@outlook.com>
2023-03-10 17:33:15 -0600

    Added tesseract

---
+++ MyTesseractApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Tesseract;
+
+var engine = new TesseractEngine("./tessdata", "eng", EngineMode.Default);
+using (var img = Pix.LoadFromFile("./example.png"))
+{
+    using (var page = engine.Process(img))
+    {
+        var text = page.GetText();
+        Console.WriteLine(text);
+    }
+}
