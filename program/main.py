76e74f25cec8b5f3ab8bf6ae26ce0487a2b2145f
connorwehrum <connorwehrum@microsoft.com>
2023-03-14 15:27:38 -0400

    Adding gitignore & updated main.py
    Ignoring OCR for now

--- program/main.py
+++ program/main.py
@@ -1,11 +1,60 @@
-from PIL import Image
-from pytesseract import pytesseract
+# bot.py
+import os
 
-# Define path to tesseract
-image = Image.open('sample-image/clash.png')
+import discord
+from dotenv import load_dotenv
+from discord import Intents
 
-text = pytesseract.image_to_string(image, lang='clash')
-print(text)
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = Intents.default()
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+client.run(TOKEN)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from PIL import Image
+# from pytesseract import pytesseract
+
+# # Define path to tesseract
+# image = Image.open('sample-image/clash.png')
+
+# text = pytesseract.image_to_string(image, lang='clash')
+# print("text")
 
 # count = text.count("the")
 # print(f"{count}")
