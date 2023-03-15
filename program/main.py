0026a0397db5073d1ccbc3e0e5a31e785c5141e8
connorwehrum <connorwehrum@microsoft.com>
2023-03-14 20:21:13 -0400

    slash commands example

--- program/main.py
+++ program/main.py
@@ -5,15 +5,60 @@ import re
 import discord
 from dotenv import load_dotenv
 from discord import Intents
-from discord.ext import commands
+from discord import app_commands
+
+MY_GUILD = discord.Object(id=349593031579533312)
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-intents = Intents.all()
-client = discord.Client(intents=intents)
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        # A CommandTree is a special type that holds all the application command
+        # state required to make it work. This is a separate class because it
+        # allows all the extra state to be opt-in.
+        # Whenever you want to work with application commands, your tree is used
+        # to store and work with them.
+        # Note: When using commands.Bot instead of discord.Client, the bot will
+        # maintain its own tree instead.
+        self.tree = app_commands.CommandTree(self)
 
+    # In this basic example, we just synchronize the app commands to one guild.
+    # Instead of specifying a guild to every command, we copy over our global commands instead.
+    # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
+    async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
+intents = discord.Intents.all()
+client = MyClient(intents=intents)
+
+@client.tree.command()
+async def hello(interaction: discord.Interaction):
+    """Says hello!"""
+    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+
+@client.tree.command()
+@app_commands.describe(
+    first_value='The first value you want to add something to',
+    second_value='The value you want to add to the first value',
+)
+async def add(interaction: discord.Interaction, first_value: int, second_value: int):
+    """Adds two numbers together."""
+    await interaction.response.send_message(f'{first_value} + {second_value} = {first_value + second_value}')
+
+# The rename decorator allows us to change the display of the parameter on Discord.
+# In this example, even though we use `text_to_send` in the code, the client will use `text` instead.
+# Note that other decorators will still refer to it as `text_to_send` in the code.
+@client.tree.command()
+@app_commands.rename(text_to_send='text')
+@app_commands.describe(text_to_send='Text to send in the current channel')
+async def send(interaction: discord.Interaction, text_to_send: str):
+    """Sends the text into the current channel."""
+    await interaction.response.send_message(text_to_send)
 
 def escape_special_chars(s):
     return re.sub(r'(?<!\\)([\'"\\()])', r'\\\1', s)
@@ -43,29 +88,25 @@ def type_message(message):
     subprocess.call(["/bin/bash", "-c", command])
 
 
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+# @client.event
+# async def on_ready():
+#     for guild in client.guilds:
+#         if guild.name == GUILD:
+#             break
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+#     print(
+#         f'{client.user} is connected to the following guild:\n'
+#         f'{guild.name}(id: {guild.id})'
+#     )
 
-from discord.ext import commands
-
-client = commands.Bot(command_prefix='/', intents=intents) # Set the command prefix to '/'
-
-@client.command()
-async def relay(ctx, *, message): # Define the relay command
-    if ctx.author == client.user:
-        return
+# @client.command()
+# async def relay(ctx, *, message): # Define the relay command
+#     if ctx.author == client.user:
+#         return
     
-    print("Message received")
-    await ctx.send(f'Sending message: "{message}" to Clash')
-    type_message(message)
+#     print("Message received")
+#     await ctx.send(f'Sending message: "{message}" to Clash')
+#     type_message(message)
 
 client.run(TOKEN)
 
