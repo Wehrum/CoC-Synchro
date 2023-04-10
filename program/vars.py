08d8175b294842a9c55bee0f4dfe57498d44d5d8
Wehrum <connor12568@gmail.com>
2023-04-10 15:13:42 -0400

    fixing vars

--- program/vars.py
+++ program/vars.py
@@ -5,6 +5,8 @@ import re
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+ENDPOINT = os.getenv('ENDPOINT')
+FORM_KEY = os.getenv('FORM_KEY')
 
 image_loading="https://github.com/Wehrum/CoC-Synchro/blob/connor/changes/program/loader.gif?raw=true"
 image_checkmark="https://github.com/Wehrum/CoC-Synchro/blob/connor/changes/program/OIP.jpg?raw=true"
